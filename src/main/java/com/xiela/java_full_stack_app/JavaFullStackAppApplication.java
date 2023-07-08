15b64a3891c1478e84751609ebd1a3bea1ae44b2
nelson8013 <nelsonekpenyong@gmail.com>
2023-07-08 10:59:07 +0100

    Added User Repository

--- src/main/java/com/xiela/java_full_stack_app/JavaFullStackAppApplication.java
+++ src/main/java/com/xiela/java_full_stack_app/JavaFullStackAppApplication.java
@@ -2,13 +2,15 @@ package com.xiela.java_full_stack_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 @SpringBootApplication
+@EnableJdbcRepositories
 public class JavaFullStackAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaFullStackAppApplication.class, args);
-		//f5084cf9-a0de-42ce-8b78-0efb5aeed753 
+		//e8dd8936-fa1f-4edc-b42b-1f8f1cab8265 
 	}
 
 }

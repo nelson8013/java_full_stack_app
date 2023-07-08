59694675fc3a98b0c7677f75583f142dd0795e82
nelson8013 <nelsonekpenyong@gmail.com>
2023-07-08 09:42:23 +0100

    fixed DB for the app

---
+++ src/test/java/com/xiela/java_full_stack_app/JavaFullStackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiela.java_full_stack_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFullStackAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
